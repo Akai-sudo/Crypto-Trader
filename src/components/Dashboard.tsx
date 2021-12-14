a7c572973ad596f91e1e9dbc98dabcf5bd0f1afc
Akai-sudo <mohorluka@gmail.com>
2021-12-14 18:53:17 +0100

    fixing css

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,12 +4,10 @@ import ethereum from '../../public/assets/Ethereum.svg'
 export const Dashboard: React.FC = () => {
     return (
         <div className="Dashboard">
-            <header className="App-header">
-                <img src={ethereum} width="30%" height="30%" className="App-logo" alt="logo" />
-                <p>Hello! I am your <b>automated crypto-trading</b> bot.</p>
-                <p>Your current balance: 0$</p>
-                <p>Your money-maker of the week: </p>
-            </header>
+            <img src={ethereum} width="30%" height="30%" className="App-logo" alt="logo" />
+            <p>Hello! I am your <b>automated crypto-trading</b> bot.</p>
+            <p>Your current balance: 0$</p>
+            <p>Your money-maker of the week: </p>
         </div>
     )
 }
