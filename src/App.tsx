10ffd9706cdaea2eeb96f9276524b06d4009ade2
Akai-sudo <mohorluka@gmail.com>
2021-12-14 18:29:07 +0100

    fixing css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import React, {useState} from 'react'
+import React from 'react'
 
 import {Dashboard} from "./components/Dashboard";
 import {ValueRate} from "./components/ValueRate";
