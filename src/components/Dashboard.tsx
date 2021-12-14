11df181e810c4f15f4f1b88e05a7f57d77e6c6c1
Akai-sudo <mohorluka@gmail.com>
2021-12-14 17:45:39 +0100

    changed css properties, added and modified p tags

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -9,7 +9,10 @@ function Dashboard() {
             <header className="App-header">
                 <img src={ethereum} width="30%" height="30%" className="App-logo" alt="logo" />
                 <p>Hello! I am your <b>automated crypto-trading</b> bot.</p>
-                <p>Your balance: </p>
+                <p>Your current balance: 0$</p>
+                <p>Your money-maker: </p>
+
+
 
                 <p>
                     <a
