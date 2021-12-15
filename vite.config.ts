09e685c694dcbe84bc68a6d707899bdc073c9f39
matic <matic.ticar@gmail.com>
2021-12-15 22:56:58 +0100

    proxy bitstamp api to localhost

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "https://www.bitstamp.net/api/v2",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, "")
+      }
+    }
+  }
 })
