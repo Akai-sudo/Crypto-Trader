827277962e7f535af1d10267a1c9dadacce7c446
matic <matic.ticar@gmail.com>
2021-12-15 22:57:42 +0100

    fetch last ETH and BTC price

--- src/components/ValueRate.tsx
+++ src/components/ValueRate.tsx
@@ -1,11 +1,25 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 export const ValueRate: React.FC = () => {
+    const [priceBTC, setPriceBTC] = useState()
+    const [priceETH, setPriceETH] = useState()
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/ticker/btceur")
+            .then(res => res.json())
+            .then(data => setPriceBTC(data.last))
+
+        fetch("http://localhost:3000/api/ticker/etheur")
+            .then(res => res.json())
+            .then(data => setPriceETH(data.last))
+    }, [])
+
     return (
         <div className="valueRate">
             <h2>Real-time crypto fluctuations: </h2>
-            <p>Current Ethereum value is: </p>
-            <p>Current Bitcoin value is: </p>
+            <p>Current Ethereum value is: {priceETH} EUR</p>
+            <p>Current Bitcoin value is: {priceBTC} EUR</p>
         </div>
     )
 }
+
