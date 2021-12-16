404cdf0d7ac44128260eda550c60f7115a75f104
Akai-sudo <mohorluka@gmail.com>
2021-12-16 01:34:22 +0100

    added another api, other modifications

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,13 +2,28 @@ import React from "react";
 import ethereum from '/assets/Ethereum.svg'
 
 export const Dashboard: React.FC = () => {
+    //placeholder variables
+    let moneyMaker = NaN;
+    let currentBalance = 0;
+    const currency = ["EUR", "USD"]
+    const usrName = "Matic"
+
+    if(currentBalance == 0) {
+        return (
+        <div className="Dashboard">
+            <img src={ethereum} width="100px" height="100px" className="App-logo" alt="logo" />
+            <h1>Hello {usrName}!</h1> <p>I am your <b>automated crypto-trading</b> bot.</p>
+            <p>Your current balance is {currentBalance} {currency[0]}. <a><b>Please top up!</b></a></p>
+        </div>
+        )
+    }
+
     return (
         <div className="Dashboard">
             <img src={ethereum} width="100px" height="100px" className="App-logo" alt="logo" />
-            <p>Hello! I am your <b>automated crypto-trading</b> bot.</p>
-            <p>Your current balance: 0$</p>
-            <p>Your money-maker of the week: </p>
-
+            <h1>Hello {usrName}!</h1> <p>I am your <b>automated crypto-trading</b> bot.</p>
+            <p>Your current balance is {currentBalance} {currency[0]}.</p>
+            <p>Your money-maker of the week is {moneyMaker}.</p>
         </div>
     )
 }
--- src/components/ValueRate.tsx
+++ src/components/ValueRate.tsx
@@ -3,22 +3,34 @@ import React, { useEffect, useState } from "react"
 export const ValueRate: React.FC = () => {
     const [priceBTC, setPriceBTC] = useState()
     const [priceETH, setPriceETH] = useState()
+    const [priceALGO, setPriceALGO] = useState()
+
+    const [error, setError] = useState(false)
+    const [loaded, setIsLoaded] = useState(true)
 
     useEffect(() => {
         fetch("http://localhost:3000/api/ticker/btceur")
             .then(res => res.json())
             .then(data => setPriceBTC(data.last))
+            .catch(e => {console.log(e); setIsLoaded(false); setError(e);})
 
         fetch("http://localhost:3000/api/ticker/etheur")
             .then(res => res.json())
             .then(data => setPriceETH(data.last))
+            .catch(e => {console.log(e); setIsLoaded(false); setError(e);})
+
+        fetch("http://localhost:3000/api/ticker/algoeur")
+            .then(res => res.json())
+            .then(data => setPriceALGO((data.last)))
+            .catch(e => {console.log(e); setIsLoaded(false); setError(e);})
     }, [])
 
     return (
         <div className="valueRate">
-            <h2>Real-time crypto fluctuations: </h2>
-            <p>Current Ethereum value is: {priceETH} EUR</p>
-            <p>Current Bitcoin value is: {priceBTC} EUR</p>
+            <h2>Current crypto prices: </h2>
+            <p>Current Ethereum value is: {priceETH}EUR</p>
+            <p>Current Bitcoin value is: {priceBTC}EUR</p>
+            <p>Current Algorand value is: {priceALGO}EUR</p>
         </div>
     )
 }
